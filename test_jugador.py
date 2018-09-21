985be488987b6e39683d2532d21ebb3066207746
HeraclitoDeEfeso <eloscurodeefeso@gmail.com>
2018-09-21 18:55:07 -0300

    TestJugador: corregidos errores de sintaxis. Jugador pasa los test

--- test_jugador.py
+++ test_jugador.py
@@ -4,37 +4,29 @@
 import unittest
 from jugador import Jugador
 from monstruo import Monstruo
+from elemento import Elemento
 
 class TestJugador(unittest.TestCase):
 
     def setUp(self):
     	#Se crea un jugador generico para realizar las pruebas
-    	player = Jugador("Unnamed",Elemento.AGUA,Elemento.FUEGO)
-    pass
+    	self.player = Jugador("Unnamed",Elemento.AGUA,Elemento.FUEGO)
 
-    def verificando_nombre_unnamed_correctamente(self):
-		self.assertEqual("Unnamed", jugador.__nombre__)
-		
+    def test_verificando_nombre_unnamed_correctamente(self):
+        self.assertEqual("Unnamed", self.player.__nombre__)
 
-	def verificando_monstruo_creado_por_unnamed(self):
-		self.assertIsInstance(player.__monstruo__, Monstruo)
+    def test_verificando_monstruo_creado_por_unnamed(self):
+        self.assertIsInstance(self.player.__monstruo__, Monstruo)
 
-	def error_al_crear_elementos_iguales(self):
-		player_bad = Jugador("Bad",Elemento.AIRE,Elemento.AIRE)
-		assertRaises(Exception)
+    def test_error_al_crear_elementos_iguales(self):
+        self.assertRaises(Exception, Jugador, "Bad", Elemento.AIRE, Elemento.AIRE)
+        
+    def test_error_al_crear_sin_elementos(self):
+        self.assertRaises(Exception, Jugador, "Human1", Elemento.TIERRA)
+        self.assertRaises(Exception, Jugador, "Human2")
+        
+    def test_error_al_crear_sin_nombre(self):
+        self.assertRaises(Exception, Jugador, "", Elemento.AIRE, Elemento.TIERRA)
 
-	def error_al_crear_sin_elementos(self):
-		player_noelemental1 = Jugador("Human1",Elemento.TIERRA)
-		assertRaises(Exception)
-		player_noelemental2 = Jugador("Human2")
-		assertRaises(Exception)
-
-	def error_al_crear_sin_nombre(self):
-		player_noname = Jugador("",Elemento.AIRE,Elemento.TIERRA)
-		assertRaises(Exception)
-
-
-
-    if __name__=="__main__":
-
+if __name__=="__main__":
     unittest.main(argv=['first-arg-is-ignored'], verbosity=2, exit=False)
