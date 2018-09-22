211b0fee6afec79f385c068fc1dfc7894b214192
Leandro Amodey <leandroamodey@gmail.com>
2018-09-22 19:07:46 -0300

    Sumo el archivo con la interfaz. Falta implementar

---
+++ aplicacion.py
@@ -0,0 +1,23 @@
+from batalla import Batalla
+
+self.cantidad_de_jugadores = 2
+self.cantidad_de_elementos_por_jugadores = 2
+
+
+def __crear_batalla__():
+
+def __jugar_partida__(batalla):
+
+def __mostrar_resultado_final(batalla):
+
+def __mostrar_resultado_parcial__(batalla):
+
+def __menu_de_elementos__(elementos=[])
+
+def __menu_de_ataques__(jugador):
+
+def __quedan_ataques_especiales__():
+
+
+if __name__ == '__main__':
+	main()
