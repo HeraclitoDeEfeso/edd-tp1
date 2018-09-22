3c0b22aefc5b2d60c3f3709525e97faa37909d25
Leandro Amodey <leandroamodey@gmail.com>
2018-09-22 17:35:10 -0300

    Agrego mas tests a la clase monstruo. A revisar: falla test con ataque disminuido (en lugar de hacer -20% deja el valor default)

--- test_monstruo.py
+++ test_monstruo.py
@@ -17,9 +17,10 @@ class TestMonstruo(unittest.TestCase):
         self.assertEqual(Elemento.AGUA, self.monster.__elementos__[1])
         self.assertEqual(Elemento.FUEGO, self.monster.__elementos__[0])
 
-    #Aun no entiendo que hace esta funcion. Leandro Amodey
     def test_verificando_opciones_iniciales(self):
         opciones_monster = self.monster.generar_opciones()
+        self.assertEqual(opciones_monster[0], Elemento.FUEGO)
+        self.assertEqual(opciones_monster[1], Elemento.AGUA)
 
     def test_verificando_generar_ataque_basico(self):
         ataque_basico = self.monster.generar_ataque(Elemento.FUEGO)
@@ -29,10 +30,52 @@ class TestMonstruo(unittest.TestCase):
         ataque_especial = self.monster.generar_ataque_especial(Elemento.FUEGO)
         self.assertIsInstance(ataque_especial, Ataque_especial)
 
-    def test_verificando_recibir_ataque_disminuye_vida(self):
+    def test_verificando_generar_ataques_elemento_que_no_es_del_monstruo(self):
+        self.assertRaises(AssertionError, self.monster.generar_ataque, Elemento.TIERRA)
+        self.assertRaises(AssertionError, self.monster.generar_ataque_especial, Elemento.TIERRA)
+
+    def test_verificando_recibir_ataque_basico_disminuye_vida(self):
         ataque_impactado = Ataque(Elemento.AGUA)
         self.monster.recibir_ataque(ataque_impactado)
         self.assertLess(self.monster.__estado_vital__, 100)
 
+    def test_verificando_recibir_ataque_basico_aumentado(self):
+        #El monstruo es tipo fuego y agua, por ende al recibir agua deberia recibir ataque de 10+20% = 12
+        ataque_impactado = Ataque(Elemento.AGUA)
+        self.monster.recibir_ataque(ataque_impactado)
+        self.assertEqual(self.monster.__estado_vital__, 100-12)
+
+    def test_verificando_recibir_ataque_basico_disminuido(self):
+        #El monstruo es tipo fuego y agua, por ende al recibir agua deberia recibir ataque de 10-20% = 8
+        ataque_impactado = Ataque(Elemento.TIERRA)
+        self.monster.recibir_ataque(ataque_impactado)
+        self.assertEqual(self.monster.__estado_vital__, 100-8)
+
+    def test_verificando_recibir_ataque_especial_aumentado(self):
+        #El monstruo es tipo fuego y agua, por ende al recibir agua deberia recibir ataque de 15+20% = 18
+        ataque_impactado = Ataque_especial(Elemento.AGUA)
+        self.monster.recibir_ataque(ataque_impactado)
+        self.assertEqual(self.monster.__estado_vital__, 100-18)
+
+    def test_verificando_recibir_ataque_especial_disminuido(self):
+        #El monstruo es tipo fuego y agua, por ende deberia recibir ataque de 15-20% = 12
+        ataque_impactado = Ataque_especial(Elemento.TIERRA)
+        self.monster.recibir_ataque(ataque_impactado)
+        self.assertEqual(self.monster.__estado_vital__, 100-12)        
+
+
+    def test_verificando_recibir_ataque_no_elemental(self):
+        #Chequeo que en caso de que pase un ataque con elemento NADA o NULO el ataque es 0
+        #En nuestra implementacion esto nunca ocurre, ya que el jugador puede atacar solo con dos elementos o con uno.
+        #Pero se verifica para que en caso de algun error al pasar el ataque no afecte el desarrollo del juego
+        ataque_impactado = Ataque_especial(Elemento.NADA)
+        self.monster.recibir_ataque(ataque_impactado)
+        self.assertEqual(self.monster.__estado_vital__, 100)
+    """
+    Este test no tendria sentido ya que los elementos se crean internamente a partir de las opciones del menu.
+    def test_error_al_crear_sin_elementos(self):
+        self.assertRaises(AssertionError, Monstruo)
+        self.assertRaises(AssertionError, Monstruo, Elemento.AGUA)
+        """
 if __name__=="__main__":
     unittest.main(argv=['first-arg-is-ignored'], verbosity=2, exit=False)
