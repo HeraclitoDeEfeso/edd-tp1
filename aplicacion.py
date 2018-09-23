5833cdd2599953548113fa17199abe7217db74a9
HeraclitoDeEfeso <eloscurodeefeso@gmail.com>
2018-09-23 11:23:45 -0300

    Aplicación: corrijo sintaxis

--- aplicacion.py
+++ aplicacion.py
@@ -1,23 +1,31 @@
 from batalla import Batalla
 
-self.cantidad_de_jugadores = 2
-self.cantidad_de_elementos_por_jugadores = 2
+CANTIDAD_DE_JUGADORES = 2
+CANTIDAD_DE_ELEMENTOS_POR_JUGADORES = 2
 
+def main():
+    pass
 
 def __crear_batalla__():
+    pass
 
 def __jugar_partida__(batalla):
+    pass
 
 def __mostrar_resultado_final(batalla):
+    pass
 
 def __mostrar_resultado_parcial__(batalla):
+    pass
 
-def __menu_de_elementos__(elementos=[])
+def __menu_de_elementos__(elementos=[]):
+    pass
 
 def __menu_de_ataques__(jugador):
+    pass
 
 def __quedan_ataques_especiales__():
-
+    pass
 
 if __name__ == '__main__':
 	main()
