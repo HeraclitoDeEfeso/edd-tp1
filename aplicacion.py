317bebf5d1592077d8bbbc0a6a76ac050fbe61d6
HeraclitoDeEfeso <eloscurodeefeso@gmail.com>
2018-09-25 18:56:51 -0300

    Aplicacion: modular y pythonificar

--- aplicacion.py
+++ aplicacion.py
@@ -5,156 +5,63 @@ import os
 
 CANTIDAD_DE_JUGADORES = 2
 CANTIDAD_DE_ELEMENTOS_POR_JUGADORES = 2
+NOMBRE_ARCHIVO_BATALLA = "batalla-store"
 
 def main():
+    #Limpio Pantalla
+    os.system('cls')
     while True:
-        #Limpio Pantalla
-        os.system('cls')
         menu_inicial = Menu("Bienvenido a la batalla de monstruos. Seleccione una opcion:",{"1":"Crear Partida","2":"Cargar Partida Iniciada","3":"Salir"})
         opcionMenu = menu_inicial.mostrar_y_pedir_input()
-
         if opcionMenu=="1":
-   	        menu_creacion_elementos1 = Menu("Selecciona el primer elemento para tu monstruo:",{"1":"AGUA","2":"FUEGO","3":"TIERRA","4":"AIRE","5":"Sin elemento"})
-        	menu_creacion_elementos2 = Menu("Selecciona el segundo elemento para tu monstruo:",{"1":"AGUA","2":"FUEGO","3":"TIERRA","4":"AIRE","5":"Sin elemento"})
-            os.system('cls')
-            print ("Creando jugador numero 1")
-            nombrej1 = input("Ingresa el nombre del jugador 1 y luego presiona Enter.")
-            elementoj1_1 = Elemento.NADA
-            elementoj1_2 = Elemento.NADA
-
             while True:
-                opcionElemento = menu_creacion_elementos1.mostrar_y_pedir_input()
-                if opcionElemento=="1":
-                    elementoj1_1 = Elemento.AGUA
+                try:
+                    batallaEnJuego = Batalla(*[
+                            arg for i in range(CANTIDAD_DE_JUGADORES)
+                                for arg in __cargar_jugador__(i + 1)])
                     break
-                elif opcionElemento=="2":
-                    elementoj1_1 = Elemento.FUEGO
-                    break
-                elif opcionElemento=="3":
-                    elementoj1_1 = Elemento.TIERRA
-                    break
-                elif opcionElemento=="4":
-                    elementoj1_1 = Elemento.AIRE
-                    break
-                elif opcionElemento=="5":
-                    break
-                else:
-                    print("No se ingreso ninguna opcion valida. Reintentando.")
-
-            os.system("cls")
-
-            while True:
-                opcionElemento = menu_creacion_elementos2.mostrar_y_pedir_input()
-                if opcionElemento=="1":
-                    elementoj1_2 = Elemento.AGUA
-                    break
-                elif opcionElemento=="2":
-                    elementoj1_2 = Elemento.FUEGO
-                    break
-                elif opcionElemento=="3":
-                    elementoj1_2 = Elemento.TIERRA
-                    break
-                elif opcionElemento=="4":
-                    elementoj1_2 = Elemento.AIRE
-                    break
-                elif opcionElemento=="5":
-                    break
-                else:
-                    print("No se ingreso ninguna opcion valida. Reintentando.")
-
-            os.system('cls')
-            print ("Creando jugador numero 2")
-            nombrej2 = input("Ingresa el nombre del jugador 2 y luego presiona Enter.")
-            elementoj2_1 = Elemento.NADA
-            elementoj2_2 = Elemento.NADA
-
-            while True:
-                opcionElemento = menu_creacion_elementos1.mostrar_y_pedir_input()
-                if opcionElemento=="1":
-                    elemento2_1 = Elemento.AGUA
-                    break
-                elif opcionElemento=="2":
-                    elemento2_1 = Elemento.FUEGO
-                    break
-                elif opcionElemento=="3":
-                    elemento2_1 = Elemento.TIERRA
-                    break
-                elif opcionElemento=="4":
-                    elemento2_1 = Elemento.AIRE
-                    break
-                elif opcionElemento=="5":
-                    break
-                else:
-                    print("No se ingreso ninguna opcion valida. Reintentando.")
-
-            os.system("cls")
-            while True:
-                opcionElemento = menu_creacion_elementos2.mostrar_y_pedir_input()
-                if opcionElemento=="1":
-                    elemento2_2 = Elemento.AGUA
-                    break
-                elif opcionElemento=="2":
-                    elemento2_2 = Elemento.FUEGO
-                    break
-                elif opcionElemento=="3":
-                    elemento2_2 = Elemento.TIERRA
-                    break
-                elif opcionElemento=="4":
-                    elemento2_2 = Elemento.AIRE
-                    break
-                elif opcionElemento=="5":
-                    break
-                else:
-                    print("No se ingreso ninguna opcion valida. Reintentando.")
-
-            try:
-            	batallaEnJuego = Batalla(nombrej1,elementoj1_1,elementoj1_2,nombrej2,elementoj2_1,elementoj2_2)
-
-            except Exception:
-
-            __jugar_turno__(batallaEnJuego)
-
-
-
-
+                except Exception as e:
+                    print("Surgio un problema: %s" % e)
+            break
         elif opcionMenu=="2":
-
             print ("")
-
             input("Has pulsado la opción 2...\npulsa una tecla para continuar")
-
         elif opcionMenu=="3":
             input("Saliendo de la batalla. \n Hasta luego!")
-            break
-
+            return
         else:
             input("De verdad no podes ingresar un simple numero...?\nPulsa una tecla cualquiera para reintentar")
+    __jugar_turno__(batallaEnJuego)
+        
+def __cargar_jugador__(indice):
+    print ("Creando jugador numero %d" % indice)
+    return  (input("Ingresa el nombre del jugador 1 y luego presiona [Enter]: "),
+            *(__elegir_elemento__(
+                    Menu("Selecciona el %d° elemento para tu monstruo:" % (i + 1),
+                         {"1" : "AGUA",
+                          "2" : "FUEGO",
+                          "3" : "TIERRA",
+                          "4" : "AIRE",
+                          "5" : "Sin elemento"}))
+                    for i in range(CANTIDAD_DE_ELEMENTOS_POR_JUGADORES)))
 
+def __elegir_elemento__(menu):
+    while True:
+        opcionElemento = menu.mostrar_y_pedir_input()
+        if opcionElemento=="1":
+            return Elemento.AGUA
+        elif opcionElemento=="2":
+            return Elemento.FUEGO
+        elif opcionElemento=="3":
+            return Elemento.TIERRA
+        elif opcionElemento=="4":
+            return Elemento.AIRE
+        elif opcionElemento=="5":
+            return Elemento.NADA
+        else:
+            print("No se ingreso ninguna opcion valida. Reintentando.")
 
-
-
-def __crear_batalla__():
-    pass
-
-def __guardar_partida__(batalla):
-    pass
-
-def __jugar_partida__(batalla):
-    pass
-
-def __mostrar_resultado_final__(batalla):
-    pass
-
-def __mostrar_resultado_parcial__(batalla):
-    pass
-
-def __menu_de_elementos__(elementos=[]):
-    pass
-
-def __menu_de_ataques__(jugador):
-    pass
-
-def __quedan_ataques_especiales__():
+def __jugar_turno__(batalla):
     pass
 
 if __name__ == '__main__':
