a6a224406c80c3c89efab072cddea5bf406491ad
Franco Facchini <facchinifn@gmail.com>
2018-09-26 00:12:44 -0300

    Update aplicacion.py

--- aplicacion.py
+++ aplicacion.py
@@ -62,7 +62,42 @@ def __elegir_elemento__(menu):
             print("No se ingreso ninguna opcion valida. Reintentando.")
 
 def __jugar_turno__(batalla):
-    pass
+    mi_monstruo = batalla.__jugador_atacante__.__monstruo__
+    menu_jugada = Menu("Batalla en progreso.\nSeleccione una acción:",{"1":"Realizar una jugada", "2":"Guardar Batalla actual", "3":"Salir sin guardar"})
+    menu_ataque = Menu("Seleccione el tipo de ataque a realizar:", {"1":"Ataque Normal", "2":"Ataque Especial"})
+    menu_elemento = Menu("Seleccione el elemento del monstruo con que atacar:", {"1":mi_monstruo.__elementos__[0], "2":mi_monstruo.__elementos__[1]})
+    while !(batalla.termino()):
+        opcion_jugada = menu_jugada.mostrar_y_pedir_input()
+        if(opcion_jugada == "1"):
+            opcion_ataque = menu_ataque.mostrar_y_pedir_input()
+            if(opcion_ataque == "1"):
+                opcion_elemento = menu_elemento.mostrar_y_pedir_input()
+                if(opcion_elemento == "1"):
+                    batalla.jugada(mi_monstruo.generar_ataque(menu_elemento(opcion_elemento)))
+                elif(opcion_elemento == "2"):
+                    batalla.jugada(mi_monstruo.generar_ataque(menu_elemento(opcion_elemento)))
+                else:
+                    input("Selección inválida. Reiniciando turno.")
+            elif(opcion_ataque == "2"):
+                opcion_elemento = menu_elemento.mostrar_y_pedir_input()
+                if(opcion_elemento == "1"):
+                    batalla.jugada(mi_monstruo.generar_ataque(menu_elemento(opcion_elemento)))
+                elif(opcion_elemento == "2"):
+                    batalla.jugada(mi_monstruo.generar_ataque(menu_elemento(opcion_elemento)))
+                else:
+                    input("Selección inválida. Reiniciando turno.")
+            else:
+                input("Selección inválida. Reiniciando turno.")
+        elif(opcion_jugada == "2"):
+            __guardar_partida__(batalla)
+            break
+        elif(opcion_jugada == "3"):
+            input("Saliendo de la batalla. \n Hasta luego!")
+            return
+        else:
+            input("Selección inválida. Reiniciando turno.")
+    if(batalla.termino()):
+        print(batalla.ganador())
 
 if __name__ == '__main__':
     main()
