fb06babb38de6ad67c359fcbb69d7286c0139547
HeraclitoDeEfeso <eloscurodeefeso@gmail.com>
2018-10-13 11:43:43 -0300

    Todos los modulos python van en el paquete del proyecto

---
+++ batalla/batalla.py
@@ -0,0 +1,40 @@
+from jugador import Jugador
+from elemento import Elemento
+from ataque import Ataque
+
+class Batalla(object):
+
+    def __init__(self, nombrePrimerJugador, primerElementoPrimerJugador, segundoElementoPrimerJugador, nombreSegundoJugador, primerElementoSegundoJugador, segundoElementoSegundoJugador):
+        self.elementos = [Elemento.FUEGO, Elemento.TIERRA, Elemento.AIRE, Elemento.AGUA]
+        self.__jugador_atacante__ = Jugador(nombrePrimerJugador, primerElementoPrimerJugador, segundoElementoPrimerJugador)
+        self.__jugador_defensor__ = Jugador(nombreSegundoJugador, primerElementoSegundoJugador, segundoElementoSegundoJugador)
+        if (nombrePrimerJugador == nombreSegundoJugador):
+            raise ValueError("Nombres de monstruo idénticos")
+            raise Exception("Nombres idénticos")
+
+    def __str__(self):
+        return str(self.__dict__)
+
+    def obtenerJugadorAtacante(self):
+        return (str(self.__jugador_atacante__))
+
+    def obtenerJugadorDefensor(self):
+        return (str(self.__jugador_defensor__))
+
+    def termino(self):
+        return (self.__jugador_atacante__.__monstruo__.__estado_vital__ <= 0 
+                or self.__jugador_defensor__.__monstruo__.__estado_vital__ <= 0)
+
+    def ganador(self):
+        ganador = None
+        if (self.termino()):
+            ganador = self.__jugador_defensor__
+        return ganador
+
+    def jugada(self, ataque):
+        if (not self.termino()):
+            self.__jugador_defensor__.__monstruo__.recibir_ataque(ataque)
+            auxiliar = self.__jugador_atacante__
+            self.__jugador_atacante__ = self.__jugador_defensor__
+            self.__jugador_defensor__ = auxiliar
+
---
+++ batalla/jugador.py
@@ -0,0 +1,13 @@
+from monstruo import Monstruo
+from elemento import Elemento
+class Jugador():
+
+    def __init__(self, name, elem_1, elem_2):
+        assert elem_1 != Elemento.NADA or elem_2 != Elemento.NADA, 'los dos elementos no pueden ser nulos'
+        assert elem_1 != elem_2,'los elementos deben ser diferentes'
+        assert len(name) >= 3, 'el nombre debe tener almenos 3 caracteres'
+        self.__monstruo__ = Monstruo(elem_1, elem_2)
+        self.__nombre__ = name
+        
+    def __str__(self):
+        return str(self.__dict__)
